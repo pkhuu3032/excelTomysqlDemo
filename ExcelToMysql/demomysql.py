301b15488e6f8f1c09485233d0d9a937a5c4ad03
pkhuu3032 <khuu_p@yahoo.com>
2021-12-07 10:41:26 -0600

    Update demomysql.py

--- ExcelToMysql/demomysql.py
+++ ExcelToMysql/demomysql.py
@@ -1,20 +1,23 @@
 import mysql.connector
-import xlrd
+import xlrd #excel should save as "Excel 97-2003 workbook" for this to work and not the default setting
 
 db1=mysql.connector.connect(
   host ="localhost",
-  user ="root",
-  passwd ="1234qwer",
-  database ="testdb"
+  user ="############",  #This is the mysql server username
+  passwd ="###################", #This is the mysql server password
+  database ="#################"  #This is the database name  
   )
 
+#if database doesn't have a tabe yet (use code down below, this can only work once and can't run again)
 #mycursor.execute("create table personInfo(Name varchar(30),Age smallint unsigned,Color varchar(15),Sport varchar(15),Gender varchar(1),personID int PRIMARY KEY AUTO_INCREMENT)")
+
 
 mycursor=db1.cursor()
 mylist=list()
-a=xlrd.open_workbook("Book1.xls")
+a=xlrd.open_workbook("Book1.xls") 
 sheet=a.sheet_by_index(0)
 sheet.cell_value(0,0)
+#Range can be change when data get bigger
 for i in range(1,15):
   #print(tuple(sheet.row_values(i)))
   mylist.append(tuple(sheet.row_values(i)))
@@ -22,8 +25,19 @@ for i in range(1,15):
 q="insert into personinfo(Name,Age,Color,Sport,Gender)values(%s,%s,%s,%s,%s)"
 mycursor.executemany(q,mylist)
 
-db1.commit()
+db1.commit() #This is where the data get uploaded to the server and make changes on the server
 db1.close()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
